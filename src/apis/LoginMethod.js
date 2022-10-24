0a66fa62ac07add500d3cd976fc117a89bc71ce7
kore002 <59078274+kore002@users.noreply.github.com>
2022-10-24 12:44:17 +0900

    회원가입 부분 완성
    로그인 토큰 처리 금일 수정 예정

--- src/apis/LoginMethod.js
+++ src/apis/LoginMethod.js
@@ -15,8 +15,99 @@ async function LoginPost () {
   // console.log(response.data);
 };
 
+async function EmailDuplicate  (email) {
+  try {
+      const response = await Instance.post('/api/v1/auth/email/duplicate',{
+          "email": email,
+          "type": "JOIN"
+      });
+      console.log(response.data);
+      return response;
+  } catch (error) {
+      console.log(error);
+      return error;
+  }
+  // console.log(response.data);
+};
+
+async function EmailCheck (email) {
+  try {
+      const response = await Instance.post('/api/v1/auth/email/check',{
+          "email": email,
+          "type": "JOIN"
+      });
+      console.log(response.data);
+      return response;
+  } catch (error) {
+      console.log(error);
+      return error;
+  }
+  // console.log(response.data);
+};
+
+async function EmailVerify (email, code) {
+  try {
+      const response = await Instance.post('/api/v1/auth/email/verify',{
+          "email": email,
+          "code": code
+      });
+      console.log(response.data);
+      return response;
+  } catch (error) {
+      console.log(error);
+  }
+  // console.log(response.data);
+};
+
+async function Join (name, email, password, code) {
+  try {
+      const response = await Instance.post('/api/v1/auth/join',{
+        "name": name,
+        "email": email,
+        "password": password,
+        "code": code
+      });
+      console.log(response.data);
+      return response;
+  } catch (error) {
+      console.log(error);
+  }
+  // console.log(response.data);
+};
+
+async function Code (email, password) {
+  try {
+      const response = await Instance.post('/api/v1/auth/code',{
+        "email": email,
+        "password": password,
+      });
+      console.log(response.data);
+  } catch (error) {
+      console.log(error);
+  }
+  // console.log(response.data);
+};
+
+async function Token (code) {
+  try {
+      const response = await Instance.post('/api/v1/auth/code',{
+        "grant_type": "authorization_code",
+        "code": code
+      });
+      console.log(response.data);
+  } catch (error) {
+      console.log(error);
+  }
+  // console.log(response.data);
+};
 const LoginMethod = {
-  LoginPost
+  LoginPost,
+  EmailDuplicate,
+  EmailCheck,
+  EmailVerify,
+  Join,
+  Code,
+  Token
 }
 
 export default LoginMethod;
--- src/components/login/register/RegisterForm.js
+++ src/components/login/register/RegisterForm.js
@@ -11,18 +11,29 @@ const RegisterForm = () => {
   const [password, setPassword] = useState("");
   const [passwordch, setPasswordch] = useState("");
   const [name, setName] = useState("");
-  const [id, setId] = useState("");
-  const [emailcertificationNum, setEmailCertificationNum] = useState('');
-  const [emailcheck, setEmailCheck] = useState(true);
-  const [passwordcheck, setPasswordCheck] = useState(true);
-  const [idcheck, setIdCheck] = useState(true);
+
+  const [emailcode, setEmailCode] = useState('');
+
+
+  const [emailduplicatecheck, setEmailDuplicateCheck] = useState(false); // 이메일 중복 체크 판단
+  const [emailcheck, setEmailCheck] = useState(true); // 이메일 형식 체크 판단 => 포커스 아웃이후에 형식 판단을 위해 초기값 트루
+ 
+  const [codesendcheck, setCodeSendCheck] = useState(false); // 코드 요청 보냈는지 판단 => 인증요청, 인증확인이 같은 자리의 버튼에서 일어나기에 변수로 판단
+  const [codecheck, setCodeCheck] = useState(true); // 이메일 코드 형식 체크 판단 => 포커스 아웃이후에 형식 판단을 위해 초기값 트루
+  const [coderusultcheck, setCodeResultCheck] = useState(false);
+
+  const [passwordcheck, setPasswordCheck] = useState(true); // 패스워드 형식 체크 판단
+  const [passwordsameck, setPasswordSameck] = useState(true); // 패스워드 중복 판단
+
+  const [idcheck, setIdCheck] = useState(false);
   const [min, setMin] = useState(5);
   const [sec, setSec] = useState(0);
   const [timeraction, setTimerAction] = useState(false);
   var regExpname = /^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]{2,20}$/
   var regExpid = /^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]{6,20}$/
   var regExpemail = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i
-  var regExppw = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,20}$/
+  var regExppw = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/
+  var regExpcode = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8}$/
 
   useEffect(() => {
     if (timeraction) {
@@ -32,7 +43,10 @@ const RegisterForm = () => {
         }
         if (sec === 0) {
           if (min === 0) {
-            alert('인증시간 초과')
+            alert('인증시간 초과');
+            setTimerAction(false);
+            setMin(5);
+            setSec(0);
           } else {
             setMin(min - 1);
             setSec(59);
@@ -43,10 +57,37 @@ const RegisterForm = () => {
     }
   }, [min, sec, timeraction]);
 
- const test = () => {
-  console.log('다른 함수'+emailcheck)
+
+// 회원가입 인증
+ const test = (name, email, emailcode, passwordch) => {
+  if(name == null){
+    alert('이름을 입력해 주세요')
+  }else if(email == null){
+    alert('이메일을 입력해 주세요')
+  }else if(regExpemail.test(email)){
+    alert('이메일 형식을 체크해 주세요')
+  }else if(emailcode == null){
+    alert('인증 코드를 입력해 주세요')
+  }else if(regExpcode.test(emailcode)){
+    alert('인증코드 형식을 체크해 주세요')
+  }else if(passwordch == null){
+    alert('비밀 번호와 비밀 번호 확인을 입력해 주세요')
+  }else if(password !== passwordch){
+    alert('비밀 번호가 일치하지 않습니다. 확인해 주세요')
+  }else{
+    const respone = LoginMethod.Join(name, email, passwordch, emailcode);
+    if(respone.data.userCode !== null){
+      alert('회원가입 되었습니다.');
+    }else{
+      alert('알 수 없는 오류');
+    }
+    console.log(respone);
+  }
+  
  }
 
+
+//이메일 형식 체크 판단
  const EmailChecktest = () => {
   if(regExpemail.test(email)){
     setEmailCheck(true);
@@ -56,67 +97,118 @@ const RegisterForm = () => {
     console.log(emailcheck)
   }
  }
- const EmailCertification = () => {
-  alert('인증요청을 보냈습니다')
-  setTimerAction(true)
+ //이메일 코드 형식 체크 판단
+ const CodeChecktest = () => {
+  if(regExpcode.test(emailcode)){
+    setCodeCheck(true);
+  }else{
+    setEmailCheck(false);
+  }
  }
 
- const idchecked = () => {
-  if(regExpid.test(id)){
-    setIdCheck(true);
-    alert('아이디 중복체크')
-    console.log('out :'+emailcheck)
+ //이메일 인증 코드를 보내는 동작 
+ const EmailCodeSend = async(email) => {
+  const respone = await LoginMethod.EmailCheck(email);
+  if(respone.data.result){
+    alert('인증요청을 보냈습니다')
+    setCodeSendCheck(true);
+    setTimerAction(true);
+  }else{
+    alert('오류 : '+respone)
+  }
+ }
+  //코그 인증 확인 동작
+  const EmailCodeCheck = async(email, emailcode) => {
+    const respone = await LoginMethod.EmailVerify(email, emailcode);
+    if(respone.data.result){
+      alert('코드가 일치 합니다')
+      setCodeResultCheck(true);
+      setTimerAction(false);
+    }else if(!respone.data.result){
+      alert('코드가 다릅니다')
+    }
+    else{
+      alert('오류 : '+respone)
+    }
+   }
+
+
+ //이메일 중복 체크 동작
+ const emailduplicate = async(email) => {
+  if(regExpemail.test(email)){
+    const respone = await LoginMethod.EmailDuplicate(email);
+    if(respone.data.result){
+      alert('사용가능한 이메일 입니다')
+      setEmailDuplicateCheck(true);
+      setIdCheck(true);
+    }else if( !respone.data.result ){
+      alert('중복 이메일입니다')
+    }else {
+      alert('알 수 없은 오류')
+    }
   }else{
     setIdCheck(false);
     alert('아이디 형식 오류')
-    console.log(emailcheck)
+    console.log(regExpemail.test(email))
   }
  }
+
+//비밀번호 형식 체크
  const PasswordChecktest = () => {
   if(regExppw.test(password)){
     setPasswordCheck(true);
-    if(password !== passwordch){
-      setPasswordCheck(false);
-    }
   }else{
     setPasswordCheck(false);
   }
  }
+//비밀번호 확인 체크
+const PasswordSamecheck = () => {
+  if(password === passwordch){
+    if(passwordcheck){
+      setPasswordSameck(true);
+    }
+  }else{
+    setPasswordSameck(false);
+  }
+ }
+
   return (
     <div>
       {/* <h2>시작 페이지</h2>  */}
       <div className='registerform'>
         <h2>회원가입</h2>
         <ul className='registerinfo'>
-          <li >
-            <p className='shorttext'>이름</p>
-            <input type='text' value={name} className='infoinputbox' onChange={({target: {value}}) => setName(value)}/>
+          <li>
+            <p className='textbox'>이름</p>
+            <input type='text' value={name} className='shortinfoinputbox' onChange={({target: {value}}) => setName(value)}/>
           </li>
           <li >
-            <p className='shorttext'>아이디</p>
-            <input type='text' value={id} className={idcheck ? 'idcheck' : 'unidcheck'}onChange={({target: {value}}) => setId(value)}/>
-            <button className='emailcheckbtn' onClick={(e) => idchecked()}>중복체크</button>
-          </li>
-          <li >
-            <p className='shorttext'>이메일</p>
-            <input type='text' value={email} className={emailcheck ? 'infoinputbox' : 'uninfoinputbox'}  onChange={({target: {value}}) => setEmail(value)} onBlur={() => EmailChecktest()}/>
-            {
+            <p className='textbox'>이메일</p>
+            <input type='text' value={email} className={emailcheck ? 'longinfoinputbox' : 'unlonginfoinputbox'}  onChange={({target: {value}}) => setEmail(value)} onBlur={() => EmailChecktest()}/>
+            {emailduplicatecheck ?  <button className= 'pressrdbtn'>확인 완료</button> : 
+            <button className= 'defaultbtn' onClick={(e) => {emailduplicate(email)}}>중복 확인</button>}
+            <br/>{
             emailcheck ? <p className='emailChecktext'></p> :
             ( regExpemail.test(email)
             ? <p className='emailChecktext'></p>
             : <p className='emailChecktext'>이메일 형식이 아닙니다</p>)
           }
-          </li>     
-          <li >
-            <p className='longtext'>이메일 인증</p>
-            <label className='certificationtimer' for='inputnum'>
-              <input type='text' value={emailcertificationNum} onChange={({target: {value}}) => setEmailCertificationNum(value)} className='emailcheck' id='inputnum' maxlength='6'/>
-              {min} : {sec < 10 ?  '0'+sec : sec}</label>
-            <button className={regExpemail.test(email) ? 'emailcheckbtn' : 'unemailcheckbtn'} disabled={!regExpemail.test(email)} onClick={(e) => {EmailCertification()}}>인증요청</button>
           </li>
+      
+               
           <li >
-            <p className='shorttext'>비밀번호</p>
-            <input type='password' value={password} className='infoinputbox' onChange={({target: {value}}) => setPassword(value)}/>
+            <p className='textbox'>이메일 인증</p>
+            <input type='text' value={emailcode} onChange={({target: {value}}) => setEmailCode(value)} className={codecheck ? 'shortinfoinputbox' : 'unshortinfoinputbox'} onBlur={() => CodeChecktest()} id='inputnum' maxLength='8'/>
+            <label className='certificationtimer' > {min} : {sec < 10 ?  '0'+sec : sec}</label>
+            {codesendcheck ? <button className={regExpcode.test(emailcode) ? (coderusultcheck ? 'pressrdbtn' : 'defaultbtn') : 'disabledbtn'} disabled={!regExpcode.test(emailcode)} onClick={(e) => {EmailCodeCheck(email, emailcode)}}>인증 확인</button>
+            : <button className={emailduplicatecheck ? 'defaultbtn' : 'disabledbtn'} disabled={!emailduplicatecheck} onClick={(e) => {EmailCodeSend(email)}}>인증 요청</button>
+            }
+          </li>
+
+
+          <li >
+            <p className='textbox'>비밀번호</p>
+            <input type='password' value={password} className='longinfoinputbox' onChange={({target: {value}}) => setPassword(value)} onBlur={()=>PasswordChecktest()}/>
             {
             passwordcheck ? <p className='passwordChecktext'></p> :
             ( regExpemail.test(password)
@@ -125,17 +217,16 @@ const RegisterForm = () => {
           }
           </li>
           <li >
-            <p className='longtext'>비밀번호 확인</p>
-            <input type='password' value={passwordch} className='infoinputbox' onChange={({target: {value}}) => setPasswordch(value)} onBlur={()=>PasswordChecktest()}/>
+            <p className='textbox'>비밀번호 확인</p>
+            <input type='password' value={passwordch} className='longinfoinputbox' onChange={({target: {value}}) => setPasswordch(value)} onBlur={()=>PasswordSamecheck()}/>
             {
-            passwordcheck ? <p className='passwordarticleChecktext'></p> :
-            ( password === passwordch
-            ? <p className='passwordarticleChecktext'></p>
-            : <p className='passwordarticleChecktext'>비밀번호가 다릅니다</p> )
+            passwordsameck ? 
+              <p className='passwordarticleChecktext'></p>
+            : <p className='passwordarticleChecktext'>비밀번호가 다릅니다</p>
           }
           </li>
           <li >
-            <button className='registersubmit' onClick={(e) => {test()}}>회원가입</button>
+            <button className='registersubmit' onClick={(e) => {test(name, email, emailcode, passwordch)}}>회원가입</button>
           </li>
         </ul>
       </div>
