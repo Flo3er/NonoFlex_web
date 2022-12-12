7b32fe16a509cefa182fc418c8fe8026eae7f3b8
hj.yang <makellan1926@gmail.com>
2022-12-13 00:19:03 +0900

    누락추가

---
+++ src/app/components/common/modal/PopupBox.js
@@ -0,0 +1,14 @@
+import "./PopupBox.css"
+
+const PopupBox = (props) => {
+    return (
+        <div className="popupBox">
+            <ul>
+                <li>1</li>
+                <li>2</li>
+            </ul>
+        </div>
+    );
+}
+
+export default PopupBox;
