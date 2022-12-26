63cfc543bb7f95767ee52d61ac19ab0ce4376d7e
hj.yang <makellan1926@gmail.com>
2022-12-26 21:35:34 +0900

    주소 https로 변경

--- src/apis/NonoApi.js
+++ src/apis/NonoApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const NonoAPI = axios.create({
-    baseURL: "http://3.39.53.3:3000",
+    baseURL: "https://api.nonoflex.com",
     headers: {
         Authorization : `Bearer ${sessionStorage.getItem("accessToken")}`
         }
